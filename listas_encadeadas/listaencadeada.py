a6c1f52d4141898a644468e3bdcf68c565cf6d30
Arthur Borges <arthurmine96@gmail.com>
2025-03-18 17:50:27 -0300

    listaencadeada.py

--- listas_encadeadas/listaencadeada.py
+++ listas_encadeadas/listaencadeada.py
@@ -5,59 +5,56 @@ from node import Node
 
 class ListaEncadeada:
     def __init__(self):
-        self.head = None
-        self.size = 0
+        self.head = None # cabeça
+        self._size = 0 # conta elementos
 
 
-    def append(self, element):
-        if self.head:
-            # inserção de quando a lista ja tem elementos
+    def append(self, data):
+        novo_node = Node(data)
+        if not self.head:  # Lista vazia
+            self.head = novo_node
+        else:              # Lista com elementos
             pointer = self.head
-            while(pointer.next):
+            while pointer.next:
                 pointer = pointer.next
-            pointer.next = Node(element)
-        else:
-            # primeira inserção
-            self.head = Node(element)
-        self.size = self.size + 1 
+            pointer.next = novo_node
+        self._size += 1
+        
+        def __len__(self):
+            # retorna o tamanho da lista
+            return self._size
+        
+        def get(self, index):
+            pass
+
+        def set(self, index, data):
+            pass
+        def __getitem__(self, index):
+            pointer = self.head
+            for i in range(index):
+                if pointer:
+                    pointer = pointer.next
+                else:
+                    raise IndexError("list index out of range")
+            if pointer:
+                return pointer.data
+            else:
+                raise IndexError("list index out of range")        
+        def __setitem__(self, index, data):
+            # lista[6] = 10
+            pointer = self.head
+            for i in range(index):
+                if pointer:
+                    pointer = pointer.next
+                else:
+                    raise IndexError("list index out of range")
+            if pointer:
+                pointer.data = novo_node
+            else:
+                raise IndexError("list index out of range") 
+
 
 lista = ListaEncadeada()
-lista.append(7)
+lista.append(10) # adiconar um elemento
+print ("elementos na sua lista:", lista._size ) # Teste
 
-
-# Feito com IA para entendimento do que estou fazendo
-"""from node import Node
-
-class ListaEncadeada:
-    def __init__(self):  # Corrigido o nome do método (faltava um '_')
-        self.head = None
-        self.size = 0
-
-    def append(self, element):
-        # Inserção quando a lista está vazia
-        if self.head is None:
-            self.head = Node(element)
-        else:
-            # Percorre a lista até o último elemento
-            pointer = self.head
-            while pointer.next:  # Parenteses desnecessários removidos
-                pointer = pointer.next
-            pointer.next = Node(element)
-        self.size += 1  # Forma simplificada de incremento
-
-    def __repr__(self):
-        items = []
-        pointer = self.head
-        while pointer:
-            items.append(str(pointer.data))
-            pointer = pointer.next
-        return "[" + " -> ".join(items) + "]"
-
-# Teste da lista
-if __name__ == "__main__":
-    lista = ListaEncadeada()
-    lista.append(7)
-    lista.append(12)
-    lista.append(5)
-    print(lista)  # Saída: [7 -> 12 -> 5]
-    print("Tamanho da lista:", lista.size)  # Deve mostrar 3"""
